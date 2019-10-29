faab27773ca33a4a1cf215eb71aecbeca0be0d17
artystable <arturostable@gmail.com>
2019-10-29 03:33:16 -0400

    adding public dir

---
+++ app/build/precache-manifest.dac2aa31a5c80670db0aa62033cc0305.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189a1c7cde620e76501ea8c4c67e2593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecb38be369ca8a17d3df",
+    "url": "/static/js/2.60317e7a.chunk.js"
+  },
+  {
+    "revision": "e15924076653d01058e2",
+    "url": "/static/js/main.c466ea19.chunk.js"
+  },
+  {
+    "revision": "281af0eb020f42ca03b9",
+    "url": "/static/js/runtime-main.13ac9109.js"
+  }
+]);
